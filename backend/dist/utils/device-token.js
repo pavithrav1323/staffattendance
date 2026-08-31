e741934f37555619efe569b18f97bc8964cdb563
Pavithra <pavithraannu2003@gmail.com>
2026-08-31 11:53:00 +0530

    chore: remove generated files and secrets

--- backend/dist/utils/device-token.js
+++
@@ -1,8 +0,0 @@
-import crypto from "node:crypto";
-export function hashDeviceToken(token) {
-    return crypto
-        .createHash("sha256")
-        .update(token)
-        .digest("hex");
-}
-//# sourceMappingURL=device-token.js.map
